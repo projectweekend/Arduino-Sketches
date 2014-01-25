cdca30353e1d105d6a0444a8b968a5fb93149229
Brian Hines <brian@projectweekend.net>
2014-01-25 12:56:21 -0600

    Starting a new LED matrix animation pattern

---
+++ bi_color_led_matrix_01/bi_color_led_matrix_01.ino
@@ -0,0 +1,37 @@
+#include <Wire.h>
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
+
+// color: 1 == RED
+// color: 2 == YELLOW
+// color: 3 == GREEN
+
+Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
+
+
+void animatePixel( int xPos, int yPos, int color, int speed) {
+    matrix.drawPixel(xPos, yPos, color);
+    matrix.writeDisplay();
+    delay(speed);
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("8x8 LED Matrix Test");
+  
+  matrix.begin(0x70);  // pass in the address
+}
+
+
+void loop() {
+
+    matrix.clear();
+    delay(500);
+
+    animatePixel(3, 3, 3, 300);
+    animatePixel(3, 4, 3, 300);
+    animatePixel(4, 4, 3, 300);
+    animatePixel(4, 3, 3, 300);
+
+}
