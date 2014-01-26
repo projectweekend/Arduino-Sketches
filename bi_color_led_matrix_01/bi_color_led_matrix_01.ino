42b19e8d5516925be01304cc3b755064e28d026f
Brian Hines <brian@projectweekend.net>
2014-01-25 22:47:10 -0600

    Adding more boxes to pattern

--- bi_color_led_matrix_01/bi_color_led_matrix_01.ino
+++ bi_color_led_matrix_01/bi_color_led_matrix_01.ino
@@ -9,18 +9,23 @@
 Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
 
 
-void animatePixel( int xPos, int yPos, int color, int speed) {
+void animatePixel(int xPos, int yPos, int color, int speed) {
     matrix.drawPixel(xPos, yPos, color);
     matrix.writeDisplay();
     delay(speed);
 }
 
 
+void animateSmallSquare(int xPos, int yPos, int color, int speed) {
+    matrix.fillRect(xPos, yPos, 2, 2, color);
+    matrix.writeDisplay();
+    delay(speed);    
+}
+
+
 void setup() {
   Serial.begin(9600);
-  Serial.println("8x8 LED Matrix Test");
-  
-  matrix.begin(0x70);  // pass in the address
+  matrix.begin(0x70);
 }
 
 
@@ -37,15 +42,35 @@ void loop() {
     animatePixel(4, 3, 3, 300);
 
     // Draw top left corner box
-    animatePixel(7, 0, 1, 300);
-    animatePixel(7, 1, 1, 300);
-    animatePixel(6, 1, 1, 300);
-    animatePixel(6, 0, 1, 300);
+    animatePixel(7, 0, 1, 100);
+    animatePixel(7, 1, 1, 100);
+    animatePixel(6, 1, 1, 100);
+    animatePixel(6, 0, 1, 100);
 
     // Draw top right corner box
-    animatePixel(7, 7, 1, 300);
-    animatePixel(7, 6, 1, 300);
-    animatePixel(6, 6, 1, 300);
-    animatePixel(6, 7, 1, 300);    
+    animatePixel(7, 7, 1, 100);
+    animatePixel(7, 6, 1, 100);
+    animatePixel(6, 6, 1, 100);
+    animatePixel(6, 7, 1, 100);
+
+    // Draw bottom right corner box
+    animatePixel(0, 7, 1, 100);
+    animatePixel(0, 6, 1, 100);
+    animatePixel(1, 6, 1, 100);
+    animatePixel(1, 7, 1, 100);
+
+    // Draw bottom left corner box
+    animatePixel(0, 0, 1, 100);
+    animatePixel(0, 1, 1, 100);
+    animatePixel(1, 1, 1, 100);
+    animatePixel(1, 0, 1, 100);
+
+    // Draw boxes half way between corners
+    animateSmallSquare(3, 0, 2, 400);
+    animateSmallSquare(6, 3, 2, 400);
+    animateSmallSquare(3, 6, 2, 400);
+    animateSmallSquare(0, 3, 2, 400);
+
+    delay(3000);
 
 }
